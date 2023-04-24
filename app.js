c28c7e575f9d5c671a4ff91d3cf92bb0440739b9
EnriqueContreras <franyersoncontreras@gmail.com>
2023-04-23 21:06:31 -0500

    Se desarrolla logica de Javascript para jalar información de la API de acuerdo a los parametros que indique el usuario

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-function obtenerPreguntas () {
-    fetch('https://opentdb.com/api.php?amount=10')
+// Obtenemos los valores definidos por el usuario en la lista de selección de dificultad
+let difficulty = document.getElementById("difficulty");
+difficulty.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    difficulty = e.target.value;
+    return difficulty;
+})
+// obtenemos los valores definidos por el usuario en la lista de selección de tipo de pregunta
+let type = document.getElementById("type");
+type.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    type = e.target.value;
+    return type;
+})
+// obtenemos los valores definidos por el usuario en la lista de selección de categoria
+let category = document.getElementById("category")
+category.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    category = e.target.value;
+    return category;
+})
+
+//Ahora definimos la URL de la API con los valores preterminados por el usuario antes de obtener la información
+const URL =`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
+
+function getData () {
+    console.log(URL)
+    fetch(URL)
         .then((response)=>response.json ())
         .then((data)=> {
             let questions = data.results
             console.log(questions)
         })
-
+        .catch(error => console.error(error));
 }
 
-obtenerPreguntas ();
+// definimos la función de obtener la información de la API cuando el usuario le de clic al boton Generar Trivia
+let buttomStart = document.getElementById("generar-trivia")
+buttomStart.addEventListener("click", getData);
