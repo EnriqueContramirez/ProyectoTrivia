89194eef933e0e830239860d0aeb79968647bea7
EnriqueContreras <franyersoncontreras@gmail.com>
2023-04-24 23:52:38 -0500

    Se completó formuario y se genera función que permite crear nuevos elementos HTML para escribir las posibles respuestas para el usuario

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
-// Obtenemos los valores definidos por el usuario en la lista de selección de dificultad
-let difficulty = document.getElementById("difficulty");
-difficulty.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    difficulty = e.target.value;
-    return difficulty;
-})
-// obtenemos los valores definidos por el usuario en la lista de selección de tipo de pregunta
-let type = document.getElementById("type");
-type.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    type = e.target.value;
-    return type;
-})
-// obtenemos los valores definidos por el usuario en la lista de selección de categoria
-let category = document.getElementById("category")
-category.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    category = e.target.value;
-    return category;
-})
-
-//Ahora definimos la URL de la API con los valores preterminados por el usuario antes de obtener la información
-const URL =`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
-
+let difficulty 
+let type
+let category 
+let containerOptions = document.getElementById("container_options")
 function getData () {
+    const URL =`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
     console.log(URL)
     fetch(URL)
         .then((response)=>response.json ())
         .then((data)=> {
-            let questions = data.results
-            console.log(questions)
+            let panels = data.results
+            console.log(panels)
+            containerOptions.innerHTML = panels.map((panel) =>{
+                return (
+                    `
+                    <ul class="characters_list">
+					<li>QUESTIONS</li>
+					<li>${panels.question}</li>
+					<li>${panels.incorrect_answers}</li>
+				    </ul>
+                    `
+                )
+
+            }).join('')
+
         })
         .catch(error => console.error(error));
 }
 
+function validarOpcionesCompletas () {
+    if (difficulty == 0 && type == 0 && category == 0){
+        alert("Debe seleccionar una opción valida");
+    } else {
+        getData ();
+    }
+}
+
+// Obtenemos los valores definidos por el usuario en la lista de selección de dificultad
+difficulty = document.getElementById("difficulty").value;
+document.getElementById("difficulty").addEventListener("change", (e) => {
+    console.log(e.target.value);
+    difficulty = e.target.value;
+})
+
+// Obtenemos los valores definidos por el usuario en la lista de selección de tipo de pregunta
+type = document.getElementById("type").value;
+document.getElementById("type").addEventListener("change", (e) => {
+    console.log(e.target.value);
+    type = e.target.value;
+})
+
+// Obtenemos los valores definidos por el usuario en la lista de selección de categoria
+category = document.getElementById("category").value;
+document.getElementById("category").addEventListener("change", (e) => {
+    console.log(e.target.value);
+    category = e.target.value;
+})
+
 // definimos la función de obtener la información de la API cuando el usuario le de clic al boton Generar Trivia
 let buttomStart = document.getElementById("generar-trivia")
-buttomStart.addEventListener("click", getData);
+buttomStart.addEventListener("click", validarOpcionesCompletas);
