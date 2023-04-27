b5fe0a3f770abe2f6f57e7037f03935642b63337
EnriqueContreras <franyersoncontreras@gmail.com>
2023-04-26 23:51:11 -0500

    Se generpo en JavaScript cambios en el código que permitieran que solo se muestra una pregunta a la vez con sus respectivas respuestas y que al selecionar alguna de ellas se genere la siguiente pregunta

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-let difficulty 
-let type
-let category 
-let containerOptions = document.getElementById("container_options")
+let difficulty = 0; 
+let type = 0;
+let category= 0; 
+let panels;
+let containerOptions = document.getElementById("container_options");
+let containerquestions = document.getElementById("container_questions");
 function getData () {
     const URL =`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`;
     console.log(URL)
-    fetch(URL)
+    return fetch(URL)
         .then((response)=>response.json ())
         .then((data)=> {
-            let panels = data.results
-            console.log(panels)
-            console.log(panels.question)
-            containerOptions.innerHTML = panels.map((panel) => {
-                return (
-                    `
-                    <ul class="characters_list">
-					<li>QUESTIONS</li>
-					<li>${panels[1].question}</li>
-					<li>${panels[1].incorrect_answers}</li>
-				    </ul>
-                    `
-                )
-
-            }).join('')
-
-        })
+            panels = data.results;
+            console.log(panels);
+            return panels;
+            })
         .catch(error => console.error(error));
 }
 
@@ -32,9 +21,41 @@ function validarOpcionesCompletas () {
     if (difficulty == 0 && type == 0 && category == 0){
         alert("Debe seleccionar una opción valida");
     } else {
-        getData ();
+        getData().then((data) => {
+            panels = data;
+            mostrarPregunta(0);
+        });
     }
 }
+
+function mostrarPregunta(index){
+    const pregunta = panels[index];
+    const respuestas = pregunta.incorrect_answers.concat(pregunta.correct_answer).sort();
+    containerquestions.innerHTML = `
+    <ul class="questions_list">
+        <li>QUESTION N° ${index + 1}</li>
+        <li>${pregunta.question}</li>
+        ${respuestas
+          .map(
+            (respuesta) => 
+            `<li><button onclick='mostrarSiguientePregunta(${index + 1})'>${respuesta}</button></li>`
+            )
+          .join(" ")}
+      </ul>
+    `
+}
+
+
+function mostrarSiguientePregunta(index, panels){
+    if(index >= panels.length){
+      containerquestions.innerHTML = `
+      <p>¡Trivia finalizada!</p>
+      `
+    } else {
+      mostrarPregunta(index);
+    }
+  }
+
 
 // Obtenemos los valores definidos por el usuario en la lista de selección de dificultad
 difficulty = document.getElementById("difficulty").value;
